1e601fab7138f2b345fb10e498ebbe6aacbe7212
Grabus Edin <edin.grabus.off@gmail.com>
2021-10-03 22:19:05 +0200

    End of section

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,29 @@
+using API.Data;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using API.Entities;
+using System.Linq;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public ActionResult<IEnumerable<AppUser>> GetUsers(){
+            return _context.Users.ToList();
+        }
+        // api/users/3
+        [HttpGet("{id}")]
+        public ActionResult<AppUser> GetUser(int id){
+            return _context.Users.Find(id);
+        }
+    }
+}
